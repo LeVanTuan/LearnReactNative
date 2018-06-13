a1041374db6fb444fab4381bebc16aceaad5d09c
TuanLeo <tuanleo@b120709-mb.local>
2018-06-13 13:26:43 +0700

    Section 7

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component {
 
@@ -8,17 +9,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Header headerText={'Albums!'} />
-        <Text numberOfLines={3}>
-          "React Native is like React,
-          but it uses native components instead of web components
-          as building blocks. So to understand the basic structure 
-          of a React Native app, you need to understand some of the basic
-          React concepts, like JSX, components, state, and props. 
-          If you already know React, you still need to learn some 
-          React-Native-specific stuff, like the native components.
-          This tutorial is aimed at all audiences, 
-          whether you have React experience or not.
-          {'\n'}{'\n'}Let's do this thing."</Text>
+        <AlbumList />
       </View>
     );
   }
