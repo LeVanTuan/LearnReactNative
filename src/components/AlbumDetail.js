e4cbce0c08c1bb2a7e5835eac839fd951501d54e
TuanLeo <tuanleo@b120709-mb.local>
2018-06-21 16:19:08 +0700

    up code

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,56 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
+
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
 
         </Card>
     );
 };
 
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'colum',
+        justifyContent: 'space-around',
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    }
+};
 
 export default AlbumDetail;
